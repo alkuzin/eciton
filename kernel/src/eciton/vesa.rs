aeec16b0cafa36d5e750c4d27d0d8ca71ec86442
alkuzin <alkuzindev@gmail.com>
2025-02-13 12:18:22 +0300

    feat: added fill_screen method

--- kernel/src/eciton/vesa.rs
+++ kernel/src/eciton/vesa.rs
@@ -16,13 +16,12 @@
 
 //! Contains Video Electronics Standards Association (VESA) driver declaration.
 
-use crate::eciton::{gfx::{self, FONT_CHAR_WIDTH}, MultibootInfo};
+use crate::eciton::{gfx, MultibootInfo};
 use gfx::Rgb;
 
 
-#[derive(Debug, Clone, Copy)]
-pub struct Framebuffer
-{
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Framebuffer {
     pub addr:   u64,
     pub pitch:  u32,
     pub width:  u32,
@@ -30,10 +29,8 @@ pub struct Framebuffer
     pub bpp:    u8,
 }
 
-impl Framebuffer
-{
-    pub fn new(boot_info: &MultibootInfo) -> Framebuffer
-    {
+impl Framebuffer {
+    pub fn new(boot_info: &MultibootInfo) -> Framebuffer {
         Framebuffer {
             addr:   boot_info.framebuffer_addr,
             pitch:  boot_info.framebuffer_pitch,
@@ -44,48 +41,56 @@ impl Framebuffer
     }
 }
 
-pub struct Vesa
-{
-    fb:     Framebuffer,
-    buffer: *mut u32,
+// TODO: rename to Graphics
+#[derive(Default, Clone, Copy)]
+pub struct Vesa {
+    pub fb: Framebuffer,
 }
 
-impl Vesa
-{
-    pub fn new(boot_info: &MultibootInfo) -> Vesa
-    {
-        let fb     = Framebuffer::new(boot_info);
-        let buffer = fb.addr as *mut u32;
+impl Vesa {
+    pub fn new(boot_info: &MultibootInfo) -> Vesa {
+        Vesa { fb: Framebuffer::new(boot_info) }
+    }
 
-        Vesa { fb, buffer }
+    pub fn set(&mut self, boot_info: &MultibootInfo) {
+        self.fb = Framebuffer::new(boot_info);
     }
 
     #[inline]
-    pub fn put_pixel(&self, x: u32, y: u32, color: Rgb)
-    {
+    pub fn put_pixel(&self, x: u32, y: u32, color: Rgb) {
         if x < self.fb.width && y < self.fb.height {
             unsafe {
-                *self.buffer.offset((y * self.fb.width + x) as isize) = color;
+                let buffer = self.fb.addr as *mut u32;
+                *buffer.offset((y * self.fb.width + x) as isize) = color;
             }
         }
     }
 
-    pub fn draw_char(&self, c: char, x: u32, y: u32, fg: Rgb)
-    {
+    pub fn draw_char(&self, c: char, x: u32, y: u32, fg: Rgb, bg: Rgb, is_bg: bool) {
         static MASK: [u8; 8] = [ 128, 64, 32, 16, 8, 4, 2, 1 ];
-
         let font_ptr         = gfx::FONT.as_ptr();
         let glyph: *const u8 = unsafe { font_ptr.add(c as usize * 16) };
 
         let mut pixel: u8;
 
         for cy in 0..gfx::FONT_CHAR_HEIGHT {
-            for cx in 0..FONT_CHAR_WIDTH {
+            for cx in 0..gfx::FONT_CHAR_WIDTH {
                 pixel = unsafe { *glyph.add(cy as usize) };
 
                 if pixel & MASK[cx as usize] != 0 {
                     self.put_pixel(x + cx, y + cy, fg);
                 }
+                else if is_bg {
+                    self.put_pixel(x + cx, y + cy, bg);
+                }
+            }
+        }
+    }
+
+    pub fn fill_screen(&self, color: Rgb) {
+        for y in 0..self.fb.height {
+            for x in 0..self.fb.width {
+                self.put_pixel(x, y, color);
             }
         }
     }
