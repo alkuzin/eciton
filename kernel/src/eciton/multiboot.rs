da553f3fb847ae1b9b52fc4e929ca4a45a882aad
alkuzin <alkuzindev@gmail.com>
2025-02-13 18:35:02 +0300

    refactor: small code format changes

--- kernel/src/eciton/multiboot.rs
+++ kernel/src/eciton/multiboot.rs
@@ -18,7 +18,7 @@
 
 /// Number of bytes bytes from the start of the file
 /// to search for the header.
-pub const MULTIBOOT_SEARCH: u32 = 8192;
+pub const MULTIBOOT_SEARCH: u32       = 8192;
 pub const MULTIBOOT_HEADER_ALIGN: u32 = 4;
 
 /// The magic field should contain this.
@@ -85,10 +85,9 @@ pub const MULTIBOOT_INFO_BOOT_LOADER_NAME: u32 = 0x00000200;
 pub const MULTIBOOT_INFO_APM_TABLE: u32 = 0x00000400;
 
 /// Is there video information?
-pub const MULTIBOOT_INFO_VBE_INFO: u32 = 0x00000800;
+pub const MULTIBOOT_INFO_VBE_INFO: u32         = 0x00000800;
 pub const MULTIBOOT_INFO_FRAMEBUFFER_INFO: u32 = 0x00001000;
 
-#[rustfmt::skip]
 pub type MultibootU8  = u8;
 pub type MultibootU16 = u16;
 pub type MultibootU32 = u32;
@@ -96,24 +95,19 @@ pub type MultibootU64 = u64;
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct MultibootHeader
-{
+pub struct MultibootHeader {
     /// Must be MULTIBOOT_MAGIC - see above.
     pub magic: MultibootU32,
-
     /// Feature flags.
     pub flags: MultibootU32,
-
     /// The above fields plus this one must equal 0 mod 2^32.
     pub checksum: MultibootU32,
-
     /// These are only valid if MULTIBOOT_AOUT_KLUDGE is set.
     pub header_addr:   MultibootU32,
     pub load_addr:     MultibootU32,
     pub load_end_addr: MultibootU32,
     pub bss_end_addr:  MultibootU32,
     pub entry_addr:    MultibootU32,
-
     /// These are only valid if MULTIBOOT_VIDEO_MODE is set.
     pub mode_type: MultibootU32,
     pub width:     MultibootU32,
@@ -124,8 +118,7 @@ pub struct MultibootHeader
 /// The symbol table for a.out.
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct MultibootAOutSymbolTable
-{
+pub struct MultibootAOutSymbolTable {
     pub tabsize:  MultibootU32,
     pub strsize:  MultibootU32,
     pub addr:     MultibootU32,
@@ -135,8 +128,7 @@ pub struct MultibootAOutSymbolTable
 /// The section header table for ELF.
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct MultibootELFSectionHeaderTable
-{
+pub struct MultibootELFSectionHeaderTable {
     pub num:   MultibootU32,
     pub size:  MultibootU32,
     pub addr:  MultibootU32,
@@ -146,8 +138,7 @@ pub struct MultibootELFSectionHeaderTable
 /// Union for Multiboot symbol tables.
 #[derive(Clone, Copy)]
 #[repr(C)]
-pub union MultibootSymbolTableUnion
-{
+pub union MultibootSymbolTableUnion {
     pub aout_sym: MultibootAOutSymbolTable,
     pub elf_sec:  MultibootELFSectionHeaderTable,
 }
@@ -155,8 +146,7 @@ pub union MultibootSymbolTableUnion
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 #[rustfmt::skip]
-pub enum MultibootFramebufferType
-{
+pub enum MultibootFramebufferType {
     Indexed = 0,
     Rgb     = 1,
     EgaText = 2,
@@ -165,8 +155,7 @@ pub enum MultibootFramebufferType
 /// Struct for framebuffer palette information.
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct FramebufferPalette
-{
+pub struct FramebufferPalette {
     pub addr:       MultibootU32,
     pub num_colors: MultibootU16,
 }
@@ -174,8 +163,7 @@ pub struct FramebufferPalette
 /// Struct for framebuffer RGB information.
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct FramebufferRGB
-{
+pub struct FramebufferRGB {
     pub red_field_position:   MultibootU8,
     pub red_mask_size:        MultibootU8,
     pub green_field_position: MultibootU8,
@@ -186,51 +174,38 @@ pub struct FramebufferRGB
 
 #[derive(Clone, Copy)]
 #[repr(C)]
-pub union FramebufferUnion
-{
+pub union FramebufferUnion {
     pub palette: FramebufferPalette,
     pub rgb:     FramebufferRGB,
 }
 
 #[repr(C)]
-pub struct MultibootInfo
-{
+pub struct MultibootInfo {
     /// Multiboot info version number.
     pub flags: MultibootU32,
-
     /// Available memory from BIOS.
     pub mem_lower: MultibootU32,
     pub mem_upper: MultibootU32,
-
     /// "root" partition.
     pub boot_device: MultibootU32,
-
     /// Kernel command line.
     pub cmdline: MultibootU32,
-
     /// Boot-Module list.
     pub mods_count: MultibootU32,
     pub mods_addr:  MultibootU32,
-
-    pub u: MultibootSymbolTableUnion,
-
+    pub u:          MultibootSymbolTableUnion,
     /// Memory Mapping buffer.
     pub mmap_length: MultibootU32,
     pub mmap_addr:   MultibootU32,
-
     /// Drive Info buffer.
     pub drives_length: MultibootU32,
     pub drives_addr:   MultibootU32,
-
     // ROM configuration table.
     pub config_table: MultibootU32,
-
     // Boot Loader Name.
     pub boot_loader_name: MultibootU32,
-
     // APM table.
     pub apm_table: MultibootU32,
-
     /// Video.
     pub vbe_control_info:   MultibootU32,
     pub vbe_mode_info:      MultibootU32,
@@ -249,8 +224,7 @@ pub struct MultibootInfo
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct MultibootColor
-{
+pub struct MultibootColor {
     pub red:   MultibootU8,
     pub green: MultibootU8,
     pub blue:  MultibootU8,
@@ -258,19 +232,17 @@ pub struct MultibootColor
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u32)]
-pub enum MultibootMemoryType
-{
-    Available = 1,
-    Reserved = 2,
+pub enum MultibootMemoryType {
+    Available       = 1,
+    Reserved        = 2,
     AcpiReclaimable = 3,
-    Nvs = 4,
-    BadRam = 5,
+    Nvs             = 4,
+    BadRam          = 5,
 }
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C, packed)]
-pub struct MultibootMmapEntry
-{
+pub struct MultibootMmapEntry {
     pub size:  MultibootU32,
     pub addr:  MultibootU64,
     pub len:   MultibootU64,
@@ -279,15 +251,12 @@ pub struct MultibootMmapEntry
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct MultibootModList
-{
+pub struct MultibootModList {
     /// The memory used goes from bytes ’mod_start’ to ’mod_end-1’ inclusive.
     pub mod_start: MultibootU32,
     pub mod_end:   MultibootU32,
-
     /// Module command line.
     pub cmdline: MultibootU32,
-
     /// padding to take it to 16 bytes (must be zero).
     pub pad: MultibootU32,
 }
@@ -295,8 +264,7 @@ pub struct MultibootModList
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
 // APM BIOS info.
-pub struct MultibootApmInfo
-{
+pub struct MultibootApmInfo {
     pub version:     MultibootU16,
     pub cseg:        MultibootU16,
     pub offset:      MultibootU32,
