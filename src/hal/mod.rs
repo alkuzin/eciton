e6a6c7d90ecaab3760efd8d935551bf4d9d078e9
alkuzin <alkuzindev@gmail.com>
2025-05-25 19:00:34 +0300

    [feat]: added HAL (Hardware Abstract Layer) module

---
+++ src/hal/mod.rs
@@ -0,0 +1,8 @@
+// Project name: Eciton.
+// Description: Experimental exokernel.
+// Licence: GPL-3.0.
+// Author: Alexander (@alkuzin).
+
+//! HAL (Hardware Abstraction Layer) main module.
+
+pub mod uart;
